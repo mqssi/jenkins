d5c7382a47cfa4f466f726f3ab7e3669015b0931
mmf <mmf@DESKTOP-M78VGNE>
2019-03-17 23:23:38 +0100

    Delete final for getMessage

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -28,7 +28,7 @@ public class App
      * Récupère la constante du message à afficher
      * @return Le message Hello World!
      */
-    private final String getMessage() {
+    private String getMessage() {
         return message;
     }
 }
